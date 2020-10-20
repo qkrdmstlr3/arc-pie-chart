432d497f61f84054cd0022ebcaf1a8f029fbee7d
박은식 <qkrdmstlr3@naver.com>
2020-10-20 15:30:53 +0900

    <FEAT>: add react example

--- examples/chart-example/src/App.js
+++ examples/chart-example/src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./App.css";
+import makePieChart from "arc-pie-chart";
+import { data } from "./data";
 
 function App() {
-  return <div className="App">hello</div>;
+  const svg = useRef(null);
+
+  useEffect(() => {
+    if (svg.current) {
+      svg.current.appendChild(makePieChart(data));
+    }
+  });
+
+  return <div ref={svg} />;
 }
 
 export default App;
---
+++ examples/chart-example/src/data.js
@@ -0,0 +1,92 @@
+export const data = [
+  {
+    name: "수입",
+    percentage: 55,
+    color: "crimson",
+    textColor: "white",
+    data: [
+      {
+        name: "월급",
+        percentage: 30,
+        color: "crimson",
+        textColor: "white",
+      },
+      {
+        name: "용돈",
+        percentage: 30,
+        color: "crimson",
+        textColor: "white",
+      },
+      {
+        name: "기타수입",
+        percentage: 40,
+        color: "crimson",
+        textColor: "white",
+        data: [
+          {
+            name: "주식",
+            percentage: 60,
+            color: "crimson",
+            textColor: "white",
+          },
+          {
+            name: "빌린 돈",
+            percentage: 40,
+            color: "crimson",
+            textColor: "white",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "지출",
+    percentage: 45,
+    color: "#3BB6AE",
+    textColor: "black",
+    data: [
+      {
+        name: "식비",
+        percentage: 10,
+        color: "#3BB6AE",
+        textColor: "black",
+      },
+      {
+        name: "생활",
+        percentage: 20,
+        color: "#3BB6AE",
+        textColor: "black",
+      },
+      {
+        name: "쇼핑/뷰티",
+        percentage: 10,
+        color: "#3BB6AE",
+        textColor: "black",
+      },
+      {
+        name: "교통",
+        percentage: 20,
+        color: "#3BB6AE",
+        textColor: "black",
+      },
+      {
+        name: "의료/건강",
+        percentage: 10,
+        color: "#3BB6AE",
+        textColor: "black",
+      },
+      {
+        name: "문화/여가",
+        percentage: 10,
+        color: "#3BB6AE",
+        textColor: "black",
+      },
+      {
+        name: "미분류",
+        percentage: 20,
+        color: "#3BB6AE",
+        textColor: "black",
+      },
+    ],
+  },
+];
